e890e8b76819b418b285b83a8137fc254b17edec
amirulirsyad <2020489528@student.uitm.edu.my>
2022-04-11 20:45:55 +0800

    login signup no firebase

---
+++ app/src/main/java/com/example/pancaranticket/loginActivity.java
@@ -0,0 +1,130 @@
+package com.example.pancaranticket;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    final int MIN_PASSWORD_LENGTH = 6;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        viewInitializations();
+    }
+
+    void viewInitializations() {
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+
+        // To show back button in actionbar
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // Checking if the input in form is valid
+    boolean validateInput() {
+
+        if (etEmail.getText().toString().equals("")) {
+            etEmail.setError("Please Enter Email");
+            return false;
+        }
+        if (etPassword.getText().toString().equals("")) {
+            etPassword.setError("Please Enter Password");
+            return false;
+        }
+
+        // checking the proper email format
+        if (!isEmailValid(etEmail.getText().toString())) {
+            etEmail.setError("Please Enter Valid Email");
+            return false;
+        }
+
+        // checking minimum password Length
+        if (etPassword.getText().length() < MIN_PASSWORD_LENGTH) {
+            etPassword.setError("Password Length must be more than " + MIN_PASSWORD_LENGTH + " characters");
+            return false;
+        }
+
+        return true;
+    }
+
+    boolean isEmailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    // Hook Click Event
+
+    public void performSignUp (View v) {
+        if (validateInput()) {
+
+            // Input is valid, here send data to your server
+
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+
+            // Here you can call you API
+            // Check this tutorial to call server api through Google Volley Library https://handyopinion.com
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this,"Login Successfull",Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+                                FirebaseDatabase db = FirebaseDatabase.getInstance("https://ict602-group-project-default-rtdb.asia-southeast1.firebasedatabase.app");
+                                String userID = currentFirebaseUser.getUid();
+                                DatabaseReference ref = db.getReference("register").child(userID);
+
+                                //test
+                                Intent intent = new Intent(LoginActivity.this,Drawer.class);
+                                startActivity(intent);
+                                //end data
+                            }
+                            else{
+                                Toast.makeText(LoginActivity.this,"Incorrect Email/Password",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
+    }
+
+    public void goToSignup(View v) {
+
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pancaranticket/signUpActivity.java
@@ -0,0 +1,173 @@
+package com.example.pancaranticket;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText etFirstName, etUserName, etEmail, etPassword, etRepeatPassword;
+    final int MIN_PASSWORD_LENGTH = 6;
+
+    private FirebaseAuth mAuth;
+    userRegister userregister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        viewInitializations();
+    }
+
+    void viewInitializations() {
+        etFirstName = findViewById(R.id.et_first_name);
+        etUserName = findViewById(R.id.et_username);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+        etRepeatPassword = findViewById(R.id.et_repeat_password);
+
+        // To show back button in actionbar
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // Checking if the input in form is valid
+    boolean validateInput() {
+        if (etFirstName.getText().toString().equals("")) {
+            etFirstName.setError("Please Enter Full Name");
+            return false;
+        }
+        if (etUserName.getText().toString().equals("")) {
+            etUserName.setError("Please Enter Username");
+            return false;
+        }
+        if (etEmail.getText().toString().equals("")) {
+            etEmail.setError("Please Enter Email");
+            return false;
+        }
+        if (etPassword.getText().toString().equals("")) {
+            etPassword.setError("Please Enter Password");
+            return false;
+        }
+        if (etRepeatPassword.getText().toString().equals("")) {
+            etRepeatPassword.setError("Please Enter Repeat Password");
+            return false;
+        }
+
+        // checking the proper email format
+        if (!isEmailValid(etEmail.getText().toString())) {
+            etEmail.setError("Please Enter Valid Email");
+            return false;
+        }
+
+        // checking minimum password Length
+        if (etPassword.getText().length() < MIN_PASSWORD_LENGTH) {
+            etPassword.setError("Password Length must be more than " + MIN_PASSWORD_LENGTH + "characters");
+            return false;
+        }
+
+        // Checking if repeat password is same
+        if (!etPassword.getText().toString().equals(etRepeatPassword.getText().toString())) {
+            etRepeatPassword.setError("Password does not match");
+            return false;
+        }
+        return true;
+    }
+
+    boolean isEmailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    // Hook Click Event
+
+    public void performSignUp (View v) {
+        if (validateInput()) {
+
+            //Create object userRegister
+            userregister = new userRegister();
+
+            // Input is valid, here send data to your server
+            String firstName = etFirstName.getText().toString();
+            String lastName = etUserName.getText().toString();
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+            String repeatPassword = etRepeatPassword.getText().toString();
+
+            //Set value to object
+            userregister.setFullName(firstName);
+            userregister.setUserName(lastName);
+            userregister.setEmail(email);
+            userregister.setPermission("User");
+
+
+            Toast.makeText(this,"Register Success",Toast.LENGTH_SHORT).show();
+            // Here you can call you API
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseDatabase database = FirebaseDatabase.getInstance("https://ict602-group-project-default-rtdb.asia-southeast1.firebasedatabase.app");
+                                FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+                                String userID = currentFirebaseUser.getUid();
+                                Log.d("TEST", userID);
+                                DatabaseReference ref = database.getReference("register").child(userID) ; //nama table
+
+
+                                //Firebase insert data
+                                ref.addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        ref.setValue(userregister);
+                                        Log.d("ABC", "Firebase SUCCESS");
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+                                        //TO BE COMPLETE
+                                        Log.w("ABCD", "FIREBASE FAILED");
+                                    }
+                                });
+                                //firebase insert data
+
+
+                                // Sign in success, update UI with the signed-in user's information
+                                //Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                                startActivity(intent);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("TEST", "createUserWithEmail:failure", task.getException());
+                                //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                //Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    });
+
+        }
+    }
+
+}
